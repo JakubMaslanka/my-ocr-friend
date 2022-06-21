b0557c7de27a1eaf8518b4008122821ac8135617
unknown <kuba.ath51@gmail.com>
2022-06-22 01:47:49 +0200

    config: enable jit mode in TailwindCSS

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,25 +1,24 @@
-const defaultTheme = require('tailwindcss/defaultTheme');
+const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
-  darkMode: "class",
-  theme: {
-    extend: {
-      screens: {
-        'xxs': '380px',
-        ...defaultTheme.screens,
-      },
-    },
-  },
-  plugins: [],
-  variants: {
-    extend: {
-      backgroundColor: ["checked"],
-      borderColor: ["checked"],
-      inset: ["checked"],
-      zIndex: ["hover", "active"],
-    },
-  },
-}
+	mode: "jit",
+	content: ["./src/**/*.{js,jsx,ts,tsx}"],
+	darkMode: "class",
+	theme: {
+		extend: {
+			screens: {
+				xxs: "380px",
+				...defaultTheme.screens
+			}
+		}
+	},
+	plugins: [],
+	variants: {
+		extend: {
+			backgroundColor: ["checked"],
+			borderColor: ["checked"],
+			inset: ["checked"],
+			zIndex: ["hover", "active"]
+		}
+	}
+};
